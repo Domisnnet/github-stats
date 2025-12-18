4249d6f8a43037aed353d69f424eac95e2b7726b
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2025-12-18 18:18:19 +0000

    alterado

--- functions/main.py
+++ functions/main.py
@@ -2,18 +2,17 @@ from firebase_functions import https_fn
 from firebase_functions.options import set_global_options
 from firebase_admin import initialize_app, firestore
 
-# Limite de instâncias (ok manter)
+# Limite de instâncias
 set_global_options(max_instances=10)
 
-# Inicializa o Firebase Admin SDK
-initialize_app()
-
-# Cliente do Firestore
-db = firestore.client()
+# Inicialização segura
+app = initialize_app()
 
 @https_fn.on_request()
 def getItems(req):
     try:
+        db = firestore.client()
+
         docs = db.collection("items").stream()
         data = []
 
